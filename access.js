3a79eb145aa3de0270d62c392fc829524f4d1b90
wakudokidiary <77178772+wakudokidiary@users.noreply.github.com>
2021-03-31 22:16:44 +0900

    Add files via upload

--- access.js
+++ access.js
@@ -12,5 +12,26 @@ $('#inageya').click(function(){
   $('#glass').slideDown();
  });
 
+   // ここからメニュー
+   $('#bar').click(function(){
+    $('#menu').slideDown();
+  });
+
+  $('#bar').click(function(){
+    $(this).fadeOut();
+  });
+
+  $('#batsu').click(function(){
+    $('#menu').slideUp();
+  });
+
+
+
+  $('#batsu').click(function(){
+    $('#bar').fadeIn();
+  });
+
+// ここまでメニュー
+
 
 });
